ca848477271213c28a041a15ccf92d6c9c719be8
karkro <karol.krolikowski@gmail.com>
2020-04-03 13:27:52 +0200

    Implementing the Checking Account Debit Card

--- BankAccountApp/src/bankAccountApp/Checking.java
+++ BankAccountApp/src/bankAccountApp/Checking.java
@@ -2,20 +2,27 @@ package bankAccountApp;
 
 public class Checking extends Account{
 	// List properties specific to the Checking account
-	int debitCardNumber;
+	long debitCardNumber;
 	int debitCardPin;
 	
 	// Constructor to initialize checking account properties
 	public Checking(String name, String ssn, double initDeposit) {
 		super(name, ssn, initDeposit);
 		accountNumber = "2" + accountNumber;
-		// System.out.println("ACCOUNT NUMBER: " + accountNumber);
-		// System.out.println("NEW CHECKING ACCOUNT");
+		setDebitCard();
+	}
+	
+	private void setDebitCard() {
+		debitCardNumber = (long) (Math.random() * Math.pow(10, 12));
+		debitCardPin = (int) (Math.random() * Math.pow(10, 4));
 	}
 	
 	// List any methods specific to the Checking account
 	public void showInfo() {
 		System.out.println("Type account: Checking");
 		super.showInfo();
+		System.out.println("Your checking account features: " + 
+		"\nCard: " + debitCardNumber + 
+		"\nPin: " + debitCardPin);
 	}
 }
